66adba802d2e8b99eebeea2b2d3081a2634ac736
BlanguerinJordan <blanguerin.jordan60@hotmail.com>
2025-07-04 12:11:01 +0200

    feat: improve page protection and login UX
    - Updated protectPages middleware to redirect authenticated users from login to profile page.
    - Added "Retour à l'Accueil" button on login.html.

--- src/middlewares/protectPages.middleware.js
+++ src/middlewares/protectPages.middleware.js
@@ -1,14 +1,17 @@
-const protectedPages = ['createads', 'profil','modifyads']; // Les pages qui nécessitent une session
-
+const protectedPages = ['createads', 'profil','modifyads'];
+const bypassPages = ["login"];
 export default function protectPage(req, res, next) {
     const page = req.params.page;
 
     if (protectedPages.includes(page)) {
         if (!req.session || (!req.session.email && !req.session.iduser)) {
             console.log(`⛔ Accès interdit à la page ${page} sans session`);
-            return res.redirect('/TrouvTout/login'); // Redirection si pas connecté
+            return res.redirect('/TrouvTout/login');
         }
+    }else if(bypassPages.includes(page)){
+        if(req.session)
+            return res.redirect("/TrouvTout/profil");
     }
 
-    next(); // Sinon on continue normalement
+    next();
 }
